b6bd9a7a4a699b15d9e612f644d39df87a4731b5
YIJEUN <yijeun.dev@gmail.com>
2024-08-14 00:05:22 +0900

    Add Fix Today
    맵 기능 제작 완료
    Scene 자식 클래스 제작완료

--- KGA_OOPConsoleProject/Monsters/Monster.cs
+++ KGA_OOPConsoleProject/Monsters/Monster.cs
@@ -1,22 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using System.Numerics;
+/* 코멘트
+ * MonsterBattle() 함수 추상화가 아니라
+ * PlayAttack()함수 / MonsterAttack()함수 를 따로 작성하고
+ * 그걸 이용해서 MosterBattle()함수를 작성하는 것이 Scene에서 사용하기 용이할 것 같음
+ * 추상화에 대해 조금 더 고민할 것
+ */
 namespace KGA_OOPConsoleProject.Monsters
 {
     // 몬스터 종류별로 가지고 있는 클래스 제작
     public abstract class Monster
     {
         // 몬스터가 가지는 변수
-        string name;
-        int maxHp;
-        int nowMp;
-        int ATK;
-        int DEF;
-        int level;
+        public string name;
+        private int maxHp;
+        public int nowHp;
+        private int ATK;
+        private int DEF;
+        public int level;
         SceneType sceneType;
+
+        Player player;
+
         public Monster(string name, int maxHp, int ATK, int DEF, int level, SceneType sceneType)
         {
             this.name = name;
@@ -36,7 +40,10 @@ namespace KGA_OOPConsoleProject.Monsters
          * 출몰장소 - Scenes 열거형 타입
          */
         // 몬스터가 가지는 함수
-        public abstract void MosterEnter();
+        public void MosterEnter()
+        {
+            Console.WriteLine($" 전방에 {name}이(가) 나타났다.");
+        }
         public abstract void MonsterBattle();
         /*
          * MonsterEnter() - 몬스터 등장 함수
---
+++ KGA_OOPConsoleProject/Monsters/herbMan.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KGA_OOPConsoleProject.Monsters
+{
+    public class herbMan : Monster
+    {
+        public herbMan(string name, int maxHp, int nowHp, int ATK, int DEF, int level, SceneType sceneType) :base(name,maxHp,ATK,DEF,level, sceneType)
+        {
+            name = "약초꾼";
+            maxHp = 20;
+            nowHp = maxHp;
+            ATK = 1;
+            DEF = 0;
+            level = 1;
+            sceneType = SceneType.VillageMt;
+        }
+        // 몬스터가 가지는 함수
+        public override void MonsterBattle()
+        {
+
+        }
+        /*
+         * MonsterEnter() - 몬스터 등장 함수
+         * MonsterBattle() - 몬스터와의 전투 함수
+         */
+    }
+}
